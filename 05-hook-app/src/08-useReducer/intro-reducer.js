adabc47944a9eb4add26bc6387c1161c951365ac
yadiraferrer <yyfm.95@gmail.com>
2022-08-04 21:36:36 -0600

    Create Todo app

---
+++ 05-hook-app/src/08-useReducer/intro-reducer.js
@@ -0,0 +1,34 @@
+// ¿Qués es un reducer?
+// No es más que una función pura que retorna un estado.
+
+const initialState = [
+  {
+    id: 1,
+    todo: 'Aprender SOLID',
+    done: false,
+  },
+];
+
+const todoReducer = (state = initialState, action = {}) => {
+  if (action.type === '[TODO] add todo') {
+    return [...state, action.payload];
+  }
+  return state;
+};
+
+let todos = todoReducer();
+
+const newTodo = {
+  id: 2,
+  todo: 'Apernder Clean Code',
+  done: false,
+};
+
+const addTodoAction = {
+  type: '[TODO] add todo',
+  payload: newTodo,
+};
+
+todos = todoReducer(todos, addTodoAction);
+
+console.log({ state: todos });
