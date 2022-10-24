545b1d5c0be1b95e0bad0f3f8464361382467778
yadiraferrer <yyfm.95@gmail.com>
2022-10-24 13:09:46 -0600

    HeroesApp: add router and  navbar

---
+++ 07-heroes-spa/src/router/AppRouter.jsx
@@ -0,0 +1,19 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+
+import { DcPage, MarvelPage } from '../heroes';
+import { LoginPage } from '../auth';
+import { Navbar } from '../ui';
+
+export const AppRouter = () => {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="marvel" element={<MarvelPage />}></Route>
+        <Route path="dc" element={<DcPage />}></Route>
+        <Route path="login" element={<LoginPage />}></Route>
+        <Route path="/" element={<Navigate to={'/marvel'} />}></Route>
+      </Routes>
+    </>
+  );
+};
